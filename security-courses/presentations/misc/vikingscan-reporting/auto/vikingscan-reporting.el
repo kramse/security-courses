d8ba25d41a099688fb3571bc01ed6182d08140b1
hlk <hlk@5e371c9b-8735-0410-ae18-d53c61838984>
2007-10-18 16:04:34 +0000

    hack.lu talk at barcamp

---
+++ security-courses/presentations/misc/vikingscan-reporting/auto/vikingscan-reporting.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "vikingscan-reporting"
+ (lambda ()
+    (TeX-run-style-hooks
+     "sec6slides"
+     "latex2e"
+     "foils10"
+     "foils"
+     "18pt"
+     "landscape"
+     "a4paper")))
+

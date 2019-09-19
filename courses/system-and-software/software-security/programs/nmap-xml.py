44be22d2d5850972ddb4e5eab3ade1e9567f92de
Henrik Lund Kramshoej <hlk@kramse.org>
2019-09-19 16:11:58 +0200

    Test files

---
+++ courses/system-and-software/software-security/programs/nmap-xml.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# Read Nmap file from XML
+# Uses the ElementTree module
+
+import xml.etree.ElementTree as ET
+tree = ET.parse('testfile.xml')
+root = tree.getroot()
+
+# all item attributes
+#print('\nAll attributes:')
+#for elem in root:
+#    for subelem in elem:
+#        print(subelem.tag)
+#        print(subelem.attrib)
+
+print(root.tag)
+print('Nmap version: {:s} '.format(root.attrib['version']))
+print('Nmap started: {:s} '.format(root.attrib['startstr']))
+print('Nmap command line: {:s} '.format(root.attrib['args']))
+
+#print (root.find('./host').attrib)
+
+hosts = tree.findall('./host')
+for host in hosts:
+    print(host.tag)
+    print(host.attrib)
+    for hostvalues in host:
+        print(hostvalues.tag)
+        print(hostvalues.attrib)
+        for address in host.find('./address'):
+            print(port.tag)
+            print(port.attrib)
+        for port in host.find('./ports'):
+            print(port.tag)
+            print(port.attrib)
+#            print(port.attrib['proto'])
